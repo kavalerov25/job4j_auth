d041f415ed89ad827e5bb9675e02ff793c2bebd9
kavalerov25 <paradise_777@mail.ru>
2023-02-21 12:23:52 +0300

    1. RESTFul. Описание архитектуры [#6884]

---
+++ src/main/java/ru/job4j/domain/Person.java
@@ -0,0 +1,20 @@
+package ru.job4j.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String login;
+    private String password;
+}
