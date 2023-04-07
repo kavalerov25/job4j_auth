791440393cbb3cda87bbbd4752ec8d01acb4d176
kavalerov25 <paradise_777@mail.ru>
2023-04-07 22:27:26 +0300

    3. Сборка war [#332451] подключил бд h2

--- src/test/java/ru/job4j/Job4jAuthApplicationTests.java
+++ src/test/java/ru/job4j/Job4jAuthApplicationTests.java
@@ -2,8 +2,10 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class Job4jAuthApplicationTests {
 
 	@Test
